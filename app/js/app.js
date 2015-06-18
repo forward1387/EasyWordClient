0956f3088c66eba645a9f17983d2a318a91074fe
Yuriy_Oprysk <Yuriy_Oprysk@epam.com>
2015-06-18 11:42:44 +0300

    Initialise base structure of angular app

--- app/js/app.js
+++ app/js/app.js
@@ -1,12 +1,15 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
+var app = angular.module('EasyWordApp', [
+  'ngRoute'
+]);
+
+
+app.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.
+      when('/home', {templateUrl: 'templates/home.templ.html'}).
+      when('/login', {templateUrl: 'templates/login.templ.html'}).
+      when('/words', {}).
+      otherwise({redirectTo: '/home'});
 }]);
