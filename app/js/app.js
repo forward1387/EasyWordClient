94db8226d0cbb4bed37ca52eba30927a97693604
Yuriy_Oprysk <Yuriy_Oprysk@epam.com>
2015-06-24 17:15:07 +0300

    fix loading issue

--- app/js/app.js
+++ app/js/app.js
@@ -3,8 +3,4 @@
 // Declare app level module which depends on views, and components
 angular.module('EasyWordApp', [
   'ngRoute'
-]).config(['g', function($routeProvider) {
-  $routeProvider.
-      when('/words', {}).
-      otherwise({redirectTo: '/home'});
-}]);
+]);
---
+++ app/js/controllers/MainControllers.js
@@ -0,0 +1,3 @@
+angular.module('EasyWordApp').controller('MainController', function($scope){
+
+});
